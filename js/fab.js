531bf7e8bcd5ed6ca95dc8b4991523542241e063
Rana Coskun <coskunrana13@gmail.com>
2024-01-24 10:32:09 +0100

    fix deployment problem

--- js/fab.js
+++ js/fab.js
@@ -2,26 +2,3 @@
 //     setDefaultStyle();
 // });
 
-function changeStyle(styleSheet) {
-    let getThemeLink = document.querySelector('link#theme');
-    getThemeLink.href = styleSheet;
-
-    localStorage.setItem('chosenStylesheet', styleSheet);
-    document.getElementById('container-floating').classList.add('active');
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    setDefaultStyle();
-
-    var isFabButtonActive = localStorage.getItem('fabButtonActive');
-    var chosenStylesheet = localStorage.getItem('chosenStylesheet');
-
-    if (isFabButtonActive === 'true' && chosenStylesheet) {
-        document.getElementById('container-floating').classList.add('active');
-        changeStyle(chosenStylesheet);
-    }
-});
-
-function setDefaultStyle() {
-    changeStyle('../css/minimalism.css');
-}
