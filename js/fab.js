9e7cf8d8a00e9b0513ceed825ecc28e321f27b25
Rana Coskun <coskunrana13@gmail.com>
2024-01-24 11:11:24 +0100

    update documentation

--- js/fab.js
+++ js/fab.js
@@ -1,17 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     setDefaultStyle();
-// });
-
-function changeStyle(styleSheet) {
-    let getThemeLink = document.querySelector('link#theme');
-    getThemeLink.href = styleSheet;
-
-    localStorage.setItem('chosenStylesheet', styleSheet);
-    document.getElementById('container-floating').classList.add('active');
-}
-
 document.addEventListener('DOMContentLoaded', function () {
     setDefaultStyle();
+
+    window.onscroll = function () {
+        var header = document.querySelector(".page-header");
+        if (window.pageYOffset > 0) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    };
 
     var isFabButtonActive = localStorage.getItem('fabButtonActive');
     var chosenStylesheet = localStorage.getItem('chosenStylesheet');
@@ -20,7 +17,18 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('container-floating').classList.add('active');
         changeStyle(chosenStylesheet);
     }
+
+    initializeMap();
+
 });
+
+function changeStyle(styleSheet) {
+    let getThemeLink = document.querySelector('link#theme');
+    getThemeLink.href = styleSheet;
+
+    localStorage.setItem('chosenStylesheet', styleSheet);
+    document.getElementById('container-floating').classList.add('active');
+}
 
 function setDefaultStyle() {
     changeStyle('../css/minimalism.css');
